b0cd6ce8bff73bf2ba7acd79881f02ce4de497bb
Havoclivekiller <havoclivekiller@gmail.com>
2026-01-22 15:36:12 +0100

    reverted doubles handling, due to excessive msg updates messing with opposed test creations (race condition in the system)

--- src/scripts/doublesEveryTest/doublesEveryTest.js
+++ src/scripts/doublesEveryTest/doublesEveryTest.js
@@ -1,49 +1,20 @@
 export function registerDoublesEveryTest() {
-    Hooks.on('createChatMessage', async (message) => {
-        if (message.content.includes(game.i18n.localize("IMPMAL.Fumble")) || message.content.includes(game.i18n.localize("IMPMAL.Critical"))) {
-            return true;
-        }
-        if (!message.rolls?.[0]) return;
+    Hooks.on('renderChatMessage', (message, html, messageData) => {
+        if (game.settings.get("impmal-community", "computeDoublesAll") === true) {
+            if (message.content)
+                if (message.content.includes(game.i18n.localize("IMPMAL.Fumble")) || message.content.includes(game.i18n.localize("IMPMAL.Critical"))) {
+                    return true;
+                }
 
-        const roll = message.system?.result?.roll;
-        if ((roll % 11 !== 0) && roll !== 100) return;
-
-        let toInsert = (message.system.result.SL >= 0 && message.system.result.outcome === "success")
-            ? game.i18n.localize("IMPMAL.Critical")
-            : game.i18n.localize("IMPMAL.Fumble");
-
-        const insertHtml = `<div>${toInsert}</div>`;
-        const updated = message.content.replace(
-            '<div class="tags">',
-            `<div class="tags">${insertHtml}`
-        );
-
-        if (updated !== message.content) {
-            await message.update({ content: updated });
+            if (message.rolls[0])
+                if ((message.rolls[0].total % 11 === 0) || message.rolls[0].total === 100) {
+                    if (html[0].querySelector('.sl'))
+                        if (html[0].querySelector('.sl').textContent) {
+                            let toInsert = html[0].querySelector('.sl').textContent.includes('+') ? game.i18n.localize("IMPMAL.Critical") : game.i18n.localize("IMPMAL.Fumble");
+                            html[0].querySelector('.tags').insertAdjacentHTML('beforeend', `<div>${toInsert}</div>`)
+                        }
+                }
         }
     });
-
-    Hooks.on('updateChatMessage', async (message) => {
-        if (message.content.includes(game.i18n.localize("IMPMAL.Fumble")) || message.content.includes(game.i18n.localize("IMPMAL.Critical"))) {
-            return true;
-        }
-        if (!message.rolls?.[0]) return;
-
-        const roll = message.system?.result?.roll;
-        if ((roll % 11 !== 0) && roll !== 100) return;
-
-        let toInsert = (message.system.result.SL >= 0 && message.system.result.outcome === "success")
-            ? game.i18n.localize("IMPMAL.Critical")
-            : game.i18n.localize("IMPMAL.Fumble");
-
-        const insertHtml = `<div>${toInsert}</div>`;
-        const updated = message.content.replace(
-            '<div class="tags">',
-            `<div class="tags">${insertHtml}`
-        );
-
-        if (updated !== message.content) {
-            await message.update({ content: updated });
-        }
-    });
+}
 }
