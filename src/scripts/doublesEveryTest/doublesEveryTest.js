6f16ff21bddea9733384e94f5ee6da3cbbf164b3
Havoclivekiller <havoclivekiller@gmail.com>
2026-01-23 08:39:17 +0100

    removed renderChatMessage to reduce calls, moved doubles handling to overriding get tags calls.

--- src/scripts/doublesEveryTest/doublesEveryTest.js
+++ src/scripts/doublesEveryTest/doublesEveryTest.js
@@ -1,20 +1,68 @@
 export function registerDoublesEveryTest() {
-    Hooks.on('renderChatMessage', (message, html, messageData) => {
-        if (game.settings.get("impmal-community", "computeDoublesAll") === true) {
-            if (message.content)
-                if (message.content.includes(game.i18n.localize("IMPMAL.Fumble")) || message.content.includes(game.i18n.localize("IMPMAL.Critical"))) {
-                    return true;
-                }
 
-            if (message.rolls[0])
-                if ((message.rolls[0].total % 11 === 0) || message.rolls[0].total === 100) {
-                    if (html[0].querySelector('.sl'))
-                        if (html[0].querySelector('.sl').textContent) {
-                            let toInsert = html[0].querySelector('.sl').textContent.includes('+') ? game.i18n.localize("IMPMAL.Critical") : game.i18n.localize("IMPMAL.Fumble");
-                            html[0].querySelector('.tags').insertAdjacentHTML('beforeend', `<div>${toInsert}</div>`)
-                        }
+    let baseTestGetTags = Object.getOwnPropertyDescriptor(
+        BaseTest.prototype,
+        "tags"
+    );
+
+    Object.defineProperty(BaseTest.prototype, "tags", {
+        get() {
+            let tags = baseTestGetTags.get.call(this);
+
+            let doNotAdd = tags.some(str => str.includes(game.i18n.localize("IMPMAL.Critical")))
+                || tags.some(str => str.includes(game.i18n.localize("IMPMAL.Fumble")));
+            if (!doNotAdd) {
+                let isDoubles = this.result.roll % 11 == 0 || this.result.roll == 100;
+
+                if (isDoubles && this.result.outcome === "success") {
+                    tags.push(game.i18n.localize("IMPMAL.Critical"));
                 }
-        }
+                else if (isDoubles && this.result.outcome === "failure") {
+                    tags.push(game.i18n.localize("IMPMAL.Fumble"));
+                }
+            }
+
+            return tags;
+        },
+        configurable: true,
     });
-}
+
+    let powerTestGetTags = Object.getOwnPropertyDescriptor(
+        PowerTest.prototype,
+        "tags"
+    );
+
+    Object.defineProperty(PowerTest.prototype, "tags", {
+        get() {
+            let tags = powerTestGetTags.get.call(this);
+
+            if (tags.some(str => str.includes(game.i18n.localize("IMPMAL.Critical")))) {
+                tags.splice(tags.findIndex(s => s.includes("Critical")), 1);
+            }
+            if (tags.some(str => str.includes(game.i18n.localize("IMPMAL.Fumble")))) {
+                tags.splice(tags.findIndex(s => s.includes("Fumble")), 1);
+            }
+
+            return tags;
+        },
+        configurable: true,
+    });
+
+    let attackTestGetTags = Object.getOwnPropertyDescriptor(
+        AttackTest.prototype,
+        "tags"
+    );
+
+    Object.defineProperty(AttackTest.prototype, "tags", {
+        get() {
+            let tags = attackTestGetTags.get.call(this);
+
+            if (tags.some(str => str.includes(game.i18n.localize("IMPMAL.Fumble")))) {
+                tags.splice(tags.findIndex(s => s.includes("Fumble")), 1);
+            }
+
+            return tags;
+        },
+        configurable: true,
+    });
 }
